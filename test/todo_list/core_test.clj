61f0fd0dda42182df97dcd5d4c54198277dee8b3
FeliPython <felipe.morais@nubank.com.br>
2020-07-11 00:46:15 -0300

    Setup midje as testing library

--- test/todo_list/core_test.clj
+++ test/todo_list/core_test.clj
@@ -1,6 +1,11 @@
 (ns todo-list.core-test
   (:require [clojure.test :refer :all]
-            [todo-list.core :refer :all]))
+            [todo-list.core :refer :all]
+            [midje.sweet :refer :all]))
+(fact
+  (conj [1 2] 3) => [1 2 3]
+  (maisum 2) => 2)
+
 
 (deftest a-test
   (testing "FIXME, I fail."
